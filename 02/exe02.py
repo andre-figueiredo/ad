18c85a02d873a1149b2d04ae803888c62b8a208a
andre <andregus@gmail.com>
2017-01-10 21:36:48 -0200

    Plotting exercice 2 E[U] vs lambda

--- 02/exe02.py
+++ 02/exe02.py
@@ -11,7 +11,8 @@ import csv
 ###########################################################
 
 ### General values ----------------------------------------
-seedVal = 99998		# Seed to start simulation
+# Seed to start simulation
+seedVal = 123456
 # Time spended to attend a Customer (M/G/1 - fixed time)
 serviceTime = [0.1, 0.2, 0.3, 0.4, 0.5]
 # Maximum simulation time
@@ -20,14 +21,17 @@ maxTime = float("inf")
 numberOfSim = 1
 # [[customerName, arrival time, queue length ,time in queue, time been served, total time, end time]]
 customersData = []
+customers1Data = []
+customers2Data = []
+
 
 ### Customer one ------------------------------------------
-lamb1 = 0.62		# rate of Customer one
-NCustomer1 = 500000	# Number of Customers type one
+#lamb1 =	1.0         # rate of Customer one
+NCustomer1 = 10000	# Number of Customers type one
 priority1 = 0		# Priority number for Customer one
 ### Customer two ------------------------------------------
-lamb2 = 0.62		# rate of Customer two
-NCustomer2 = 500000	# Number of Customers type two
+#lamb2 = 1.0		    # rate of Customer two
+NCustomer2 = 10000	# Number of Customers type two
 priority2 = 0		# Priority number foR Customer two
 
 ###########################################################
@@ -76,7 +80,7 @@ class BankModel(Simulation):
             self.counter = Resource(name="Counter", unitName="John Doe", monitored=True, qType=PriorityQ, sim=self)
             s1 = Source('Source1', sim=self)
             s2 = Source('Source2', sim=self)
-            self.activate(s1, s1.generate(number=NCustomer1, interval=1.0/lamb1, typeOfClient=1 ,priority=priority1))
+            self.activate(s1, s1.generate(number=NCustomer1, interval=1.0/lamb1, typeOfClient=1, priority=priority1))
             self.activate(s2, s2.generate(number=NCustomer2, interval=1.0/lamb2, typeOfClient=2, priority=priority2))
             self.simulate(until=maxTime)
 
@@ -90,8 +94,16 @@ class BankModel(Simulation):
 ############################################################
 ## Experiment
 ############################################################
+# To get general information from a specific simulation
 bankreception = []
+
+# To iterate lambda value
+ilambda = 2.6
+
+# Array to hold information about variation of E[U]
+finalPlot = []
 for i in range(numberOfSim):
+    lamb1 = lamb2 = ilambda
     mg1 = BankModel()
     mg1.startCollection(when=maxTime, monitors=mg1.allMonitors)
     result = mg1.run(seedVal + i)
@@ -100,76 +112,84 @@ for i in range(numberOfSim):
     ############################################################
     ## Results
     ############################################################
-    customers1Data = []
-    customers2Data = []
 
-    # [[customer name, arrival time, queue length ,time in queue, time been served, total time, end time]]
-    name = 0
-    arrive = 1
-    queueLen = 2
-    wait = 3
-    timeInBank = 4
-    totalTime = 5
-    finished = 6
     # Separating Customers1 and Customers2
     for i in range(len(customersData)):
-        if customersData[i][name].startswith("Customer01"):
+        if customersData[i][0].startswith("Customer01"):
             customers1Data.append(customersData[i])
         else:
             customers2Data.append(customersData[i])
 
-    # Removing names to calculate Variances e Means
-    for i in range(len(customersData)):
-        customersData[i].pop(name)
+    # Organizing data to numPy array
+    customersData = np.array(customersData)
+    customers1Data = np.array(customers1Data)
+    customers2Data = np.array(customers2Data)
+    customersDataNoNames = customersData[:, 1:].astype(float)
+    customers1DataNoNames = customers1Data[:, 1:].astype(float)
+    customers2DataNoNames = customers2Data[:, 1:].astype(float)
 
-    for i in range(len(customers1Data)):
-        customers1Data[i].pop(name)
+    # To help index
+    # [[arrival time, queue length ,time in queue, time been served, total time, end time]]
+    arrive = 0
+    queueLen = 1
+    wait = 2
+    timeInBank = 3
+    totalTime = 4
+    finished = 5
 
-    for i in range(len(customers2Data)):
-        customers2Data[i].pop(name)
+    # Usefull informations
+    avgs = np.mean(customersDataNoNames, axis=0)
+    variances = np.var(customersDataNoNames, axis=0)
+    sumAllValues = np.sum(customersDataNoNames, axis=0)
+    sumAllValuesCustomer1 = np.sum(customers1DataNoNames, axis=0)
+    sumAllValuesCustomer2 = np.sum(customers2DataNoNames, axis=0)
 
-    avgs = np.mean(customersData, axis=0)
-    variances = np.var(customersData, axis=0)
-    #print(avgs)
-    #print(variances)
-    sumAllValues = np.sum(customersData, axis=0)
-    sumAllValuesCustomer1 = np.sum(customers1Data, axis=0)
-    sumAllValuesCustomer2 = np.sum(customers2Data, axis=0)
     # End of Simulation in minutes
     endOfSim = bankreception[0][3]
+
     # E[X_r] = E[X^2] / 2*E[X]. With E[X^2] = Var(X) + (E[X])^2
-    expResidualTime = (variances[timeInBank-2] + (avgs[timeInBank-2] ** 2)) / (2.0 * avgs[timeInBank-2])
+    expResidualTime = (variances[timeInBank] + (avgs[timeInBank] ** 2)) / (2.0 * avgs[timeInBank])
+
     # pendingService hold the E[U] calculated by simulation
-    pendingService = avgs[wait-2]
-    mi = 1.0 / avgs[timeInBank-2]
-    rho = sumAllValues[timeInBank-2] / endOfSim
-    rho1 = sumAllValuesCustomer1[timeInBank-2] / endOfSim
-    rho2 = sumAllValuesCustomer2[timeInBank-2] / endOfSim
+    pendingService = avgs[wait]
+
+    # mu, rho etc
+    mi = 1.0 / avgs[timeInBank]
+    rho = sumAllValues[timeInBank] / endOfSim
+    rho1 = sumAllValuesCustomer1[timeInBank] / endOfSim
+    rho2 = sumAllValuesCustomer2[timeInBank] / endOfSim
     pendingServiceCalc = rho * expResidualTime / (1.0 - rho)
+
+    finalPlot.append([lamb1, lamb2, lamb1+lamb2, pendingService, pendingServiceCalc])
 
     ############################################################
     ## Prints
     ############################################################
+    ####### Questao 1
+    print("###### Questao 1 #######\n")
     print("###### Informacoes gerais #########")
     print("X => v.a que mede o tempo de atendimento de um cliente")
-    print("Simulacao finalizada no minuto: %0.8f" %(endOfSim))
-    print("Numero total de clientes atendidos: %d"%(len(customersData)))
-    print("Lambda1 = %0.8f; Lambda2 = %0.8f; Lambda = %0.8f" %(lamb1,lamb2,lamb1+lamb2))
-    print("Mi = %0.8f" %(mi))
-    print("E[X] = %0.8f" %(avgs[timeInBank-2]))
-    print("Var(X) = %0.8f "%(variances[timeInBank-2]))
-    print("E[X_r] = %0.8f "%(expResidualTime))
+    print("Simulacao finalizada no minuto: %0.8f" % (endOfSim))
+    print("Numero total de clientes atendidos: %d"% (len(customersData)))
+    print("Lambda1 = %0.8f; Lambda2 = %0.8f; Lambda = %0.8f" % (lamb1, lamb2, lamb1+lamb2))
+    print("Mi = %0.8f" % (mi))
+    print("E[X] = %0.8f" % (avgs[timeInBank]))
+    print("Var(X) = %0.8f " % (variances[timeInBank]))
+    print("E[X_r] = %0.8f " % (expResidualTime))
     print("(sim) Rho1 = %0.8f" % (rho1))
     print("(sim) Rho2 = %0.8f" % (rho2))
-    print("(sim) Rho = %0.8f"%(rho))
-
-    ####### Questao 1
-    print("\n\n###### Questao 1 #######")
+    print("(sim) Rho = %0.8f" % (rho))
+    print("\n##### Resposta #######")
     print("(sim) E[U] = %0.8f"%(pendingService))
     print("(calc) E[U] = %0.8f"%(pendingServiceCalc))
-    ######## Questao 2
-    print("\n\n###### Questao 2 #######")
+    print("\n\n")
 
-"""with open("traces.csv", "w") as f:
+    # Clear customersData to the next iteration
+    customersData = []
+    customers1Data = []
+    customers2Data = []
+    ilambda += 0.1
+
+with open("traces1.1.csv", "w") as f:
     writer = csv.writer(f)
-    writer.writerows(customersData)"""
+    writer.writerows(finalPlot)
