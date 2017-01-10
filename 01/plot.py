7f0d15fe333a6b4532ce21901a1efdd31217c274
andre <andregus@gmail.com>
2017-01-10 19:54:55 -0200

    Plotting E[U] values vs lambda

--- 01/plot.py
+++ 01/plot.py
@@ -4,11 +4,11 @@ import numpy as np
 
 style.use('ggplot')
 
-l1,l2,l1l2,sim,calc = np.loadtxt('traces1.1.csv',unpack=True,delimiter=',')
+l1, l2, l1l2, sim, calc = np.loadtxt('traces1.1.csv', unpack=True, delimiter=',')
 
-plt.plot(l1l2,sim)
-plt.plot(l1l2,calc,color='g', linestyle='--')
-plt.title('lambda vs E[U] (simulado=red,calculado=green)')
+plt.plot(l1l2, sim)
+plt.plot(l1l2, calc, color='g', linestyle='--')
+plt.title('lambda vs E[U] (estimado=red, calculado=green)')
 plt.ylabel('E [U]')
 plt.xlabel('lambda1 + lambda2')
 
