df4eac4ea33a5846729bae79bf1a5d08863b80f7
andre <andregus@gmail.com>
2017-01-10 22:51:40 -0200

    Plot for exercise is not good enough

---
+++ 04/plot.py
@@ -0,0 +1,15 @@
+from matplotlib import pyplot as plt
+from matplotlib import style
+import numpy as np
+
+style.use('ggplot')
+
+l1, l2, l1l2, sim, calc = np.loadtxt('traces4.1.csv', unpack=True, delimiter=',')
+
+plt.plot(l1l2, sim)
+plt.plot(l1l2, calc, color='g', linestyle='--')
+plt.title('lambda vs E[U] (estimado=red, calculado=green)')
+plt.ylabel('E [U]')
+plt.xlabel('lambda1 + lambda2')
+
+plt.show()
